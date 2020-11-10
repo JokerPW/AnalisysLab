12301adf575c9fdad5a4af2f8cd3223ab8d92e03
Renan Spadafora "Joker" Amaral <jokerpw@gmail.com>
2020-11-10 15:07:03 -0300

    Database failed version

--- app/src/main/java/com/example/validationlab/view/DBActivity.java
+++ app/src/main/java/com/example/validationlab/view/DBActivity.java
@@ -5,20 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.validationlab.MainActivity;
 import com.example.validationlab.R;
+import com.example.validationlab.model.Analysis;
+import com.example.validationlab.utils.DatabaseAccess;
 import com.google.common.math.Stats;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DBActivity extends Activity implements View.OnClickListener/*, AdapterView.OnItemSelectedListener*/ {
+public class DBActivity extends Activity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
-    //private DatabaseAccess dba;
+    private DatabaseAccess dba;
     private Stats stats;
     private boolean isLoading;
     private List<Integer> analysis;
@@ -28,22 +32,12 @@ public class DBActivity extends Activity implements View.OnClickListener/*, Adap
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db);
 
-        //this.dba = DatabaseAccess.getInstance(this);
+        this.dba = DatabaseAccess.getInstance(this);
         isLoading = true;
         analysis = new ArrayList<Integer>();
 
         //get the spinner from the xml.
-        //this.mViewHolder.listaanalises = (Spinner) findViewById(R.id.listaanalises);
-
-
-        this.mViewHolder.addResult = (TextView) findViewById(R.id.addResult);
-        this.mViewHolder.analisysList = (TextView) findViewById(R.id.allNums);
-
-        this.mViewHolder.btnAdd = findViewById(R.id.btnAdd);
-        this.mViewHolder.btnAdd.setOnClickListener(this);
-        this.mViewHolder.btnCalculate = findViewById(R.id.btnCalculate);
-        this.mViewHolder.btnCalculate.setOnClickListener(this);
-
+        this.mViewHolder.listaanalises = (Spinner) findViewById(R.id.lista_analises);
 
         this.mViewHolder.resultadon = (TextView) findViewById(R.id.resultadon);
         this.mViewHolder.resultadomedia = (TextView) findViewById(R.id.resultadomedia);
@@ -54,44 +48,39 @@ public class DBActivity extends Activity implements View.OnClickListener/*, Adap
         this.mViewHolder.btnReturn.setOnClickListener(this);
 
 //create a list of items for the spinner.
-        //String[] items = dba.getAnalysisGroups();
+        String[] items = dba.getAnalysisGroups();
 //create an adapter to describe how the items are displayed, adapters are used in several places in android.
 //There are multiple variations of this, but this is the basic variant.
-        //if(items != null && items.length > 0) {
-            //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        if(items != null && items.length > 0) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 //set the spinners adapter to the previously created one.
-            //this.mViewHolder.listaanalises.setAdapter(adapter);
-            //this.mViewHolder.listaanalises.setOnItemSelectedListener(this);
-//        } else {
-//            this.mViewHolder.resultadon.setText("Sem dados para analisar");
-//        }
+            this.mViewHolder.listaanalises.setAdapter(adapter);
+            this.mViewHolder.listaanalises.setOnItemSelectedListener(this);
+        } else {
+            this.mViewHolder.resultadon.setText("Sem dados para analisar");
+        }
 
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnAdd) {
-            analysis.add (Integer.parseInt(this.mViewHolder.addResult.getText().toString()));
-            this.mViewHolder.addResult.setText("");
-        } else if (v.getId() == R.id.btnCalculate) {
-            getAnalysisCulture(null);
-        } else if (v.getId() == R.id.btnReturn) {
+        if (v.getId() == R.id.btnReturn) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
     }
 
-//    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-//        System.out.println("++++++++++ Item selected: " + parent.getItemAtPosition(pos));
-//        System.out.println("isLoading: " + isLoading);
-//        if (isLoading) {
-//            isLoading = false;
-//            System.out.println("$$$$$$ RETURNING HERE");
-//            return;
-//        }
-//
-//        getAnalysisCulture((String) parent.getItemAtPosition(pos));
-//    }
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        System.out.println("++++++++++ Item selected: " + parent.getItemAtPosition(pos));
+        System.out.println("isLoading: " + isLoading);
+        if (isLoading) {
+            isLoading = false;
+            System.out.println("$$$$$$ RETURNING HERE");
+            return;
+        }
+
+        getAnalysisCulture((String) parent.getItemAtPosition(pos));
+    }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
@@ -99,21 +88,17 @@ public class DBActivity extends Activity implements View.OnClickListener/*, Adap
 
 
     private void getAnalysisCulture (String group){
-//        System.out.println("@@@@@@@@ ANALYSIS SELECTED");
-//        List<Analysis> analysis = dba.getAnalysisGroup (group);
-        this.mViewHolder.analisysList.setText("");
+        System.out.println("@@@@@@@@ ANALYSIS SELECTED");
+        List<Analysis> analysis = dba.getAnalysisGroup (group);
         double total = 0;
         int[] ntotal = new int[analysis.size()];
 
         for (int i = 0; i < analysis.size(); i++) {
-//            total += analysis.get(i).getNumCultures();
-//            ntotal[i] = analysis.get(i).getNumCultures();
-            if (this.mViewHolder.analisysList.getText().length() > 0)
-                this.mViewHolder.analisysList.setText(this.mViewHolder.analisysList.getText() + ", ");
+            total += analysis.get(i).getNumCultures();
+            ntotal[i] = analysis.get(i).getNumCultures();
 
-            this.mViewHolder.analisysList.setText(this.mViewHolder.analisysList.getText() + Integer.toString(analysis.get(i)));
-            total += analysis.get(i);
-            ntotal[i] = analysis.get(i);
+            total += analysis.get(i).getNumCultures();
+            ntotal[i] = analysis.get(i).getNumCultures();
         }
         this.mViewHolder.resultadon.setText(Integer.toString(analysis.size()));
         this.mViewHolder.resultadomedia.setText(Double.toString(total / analysis.size()));
@@ -125,12 +110,7 @@ public class DBActivity extends Activity implements View.OnClickListener/*, Adap
 
 
     private static class ViewHolder {
-        //Spinner listaanalises;
-        TextView addResult;
-        Button btnAdd;
-        Button btnCalculate;
-        TextView analisysList;
-
+        Spinner listaanalises;
         TextView resultadon;
         TextView resultadomedia;
         TextView resultadomediana;
--- app/src/main/java/com/example/validationlab/view/Results.java
+++ app/src/main/java/com/example/validationlab/view/Results.java
@@ -5,14 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.validationlab.MainActivity;
 import com.example.validationlab.R;
+import com.example.validationlab.model.Analysis;
 import com.example.validationlab.utils.DatabaseAccess;
 import com.example.validationlab.utils.ImageTools;
 import com.example.validationlab.utils.SPreferences;
+
+import java.util.Calendar;
 
 public class Results extends Activity implements View.OnClickListener {
 
@@ -32,10 +36,10 @@ public class Results extends Activity implements View.OnClickListener {
         this.dba = DatabaseAccess.getInstance(this);
 
         this.mViewHolder.textBlobs = findViewById(R.id.count_blobs);
-        //this.mViewHolder.textAnalysis = findViewById(R.id.textAnalysis);
+        this.mViewHolder.textAnalysis = findViewById(R.id.text_analysis);
         this.mViewHolder.imgView = findViewById(R.id.imgvw);
-        //this.mViewHolder.btnSave = findViewById(R.id.btnSave);
-        //this.mViewHolder.btnSave.setOnClickListener(this);
+        this.mViewHolder.btnSave = findViewById(R.id.btn_save);
+        this.mViewHolder.btnSave.setOnClickListener(this);
         this.mViewHolder.btnReturn = findViewById(R.id.btnReturn);
         this.mViewHolder.btnReturn.setOnClickListener(this);
 
@@ -57,7 +61,7 @@ public class Results extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        /*if (v.getId() == R.id.btnSave) {
+        if (v.getId() == R.id.btn_save) {
             Analysis a = new Analysis();
             a.setAnalysis(this.mViewHolder.textAnalysis.getText().toString());
             a.setAnalysisDate(Calendar.getInstance().getTime());
@@ -65,7 +69,7 @@ public class Results extends Activity implements View.OnClickListener {
             a.setNumCultures(currResult);
             this.mViewHolder.btnSave.setEnabled(!dba.insertAnalysis(a));
 
-        } else*/ if (v.getId() == R.id.btnReturn) {
+        } else if (v.getId() == R.id.btnReturn) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
@@ -75,8 +79,8 @@ public class Results extends Activity implements View.OnClickListener {
     private static class ViewHolder {
         TextView textBlobs;
         ImageView imgView;
-        //EditText textAnalysis;
-        //Button btnSave;
+        EditText textAnalysis;
+        Button btnSave;
         Button btnReturn;
     }
 
